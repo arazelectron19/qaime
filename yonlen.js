1737ab80b59a2f52173c726669a4d3d2df5fe378
arazelectron19 <ssx4077@gmail.com>
2026-07-15 10:27:37 +0400

    Add files via upload

--- yonlen.js
+++ yonlen.js
@@ -2,5 +2,5 @@
 if (window.location.pathname.endsWith('index.html')) {
     // Qovluq adını qorumaq üçün yalnız index.html hissəsini təmizləyirik
     const cleanUrl = window.location.pathname.replace('index.html', '');
-    window.history.replaceState({}, document.title, cleanUrl);
+    //window.history.replaceState({}, document.title, cleanUrl);
 }
